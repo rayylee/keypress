e9d407cfad6b411f6342e5e167e061e93392b21a
rayylee <rayylee@foxmail.com>
2021-02-20 06:21:53 +0800

    remove setting key

--- src/components/body.rs
+++ src/components/body.rs
@@ -3,18 +3,11 @@ use std::{
 };
 use yew::{html, Callback, Component, ComponentLink, Html, ShouldRender};
 use yew::services::interval::{IntervalService, IntervalTask};
-use yew::services::{ConsoleService, Task};
-use wasm_bindgen::{JsCast, prelude::Closure};
-
-use web_sys::{AudioBuffer, AudioContext, AudioDestinationNode};
+use yew::services::{ConsoleService};
 
 use crate::common::msg::Msg;
 
-const SOURCE_SOUND: &[u8] = include_bytes!("../content/sound/click.wav");
-
 pub struct Body {
-    link: ComponentLink<Self>,
-    job: Option<Box<dyn Task>>,
     time: String,
     _standalone: (IntervalTask, IntervalTask),
 }
@@ -46,8 +39,6 @@ impl Component for Body {
         );
 
         Self {
-            link,
-            job: None,
             time: Body::get_current_time(),
             _standalone: (standalone_handle, clock_handle),
         }
@@ -55,41 +46,6 @@ impl Component for Body {
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::ButtonStart => {
-                self.job = None;
-                ConsoleService::info("> Button [Start] pressed.");
-
-                let show_str = format!("> SOURCE_SOUND len is: {:?}", SOURCE_SOUND.len());
-                ConsoleService::info(&show_str);
-
-                let array_u8: js_sys::Uint8Array = js_sys::Uint8Array::from(SOURCE_SOUND);
-
-                let array_buf: js_sys::ArrayBuffer = array_u8.buffer();
-
-                let audio_ctx = AudioContext::new().unwrap();
-
-                let song = audio_ctx.create_buffer_source().unwrap();
-                let destination: AudioDestinationNode = audio_ctx.destination();
-
-                let handler = move |buf: AudioBuffer| {
-                    let buffer: Option<&AudioBuffer> = Some(&buf);
-
-                    song.set_buffer(buffer);
-                    song.connect_with_audio_node(destination.as_ref()).unwrap();
-                    song.start().unwrap();
-                };
-
-                let handle: Box<dyn FnMut(_) + 'static> = Box::new(handler) as Box<dyn FnMut(_)>;
-
-                let cb = Closure::wrap(handle);
-
-                audio_ctx.decode_audio_data_with_success_callback(&array_buf,
-                    cb.as_ref().unchecked_ref()).unwrap();
-
-                cb.forget();
-
-                true
-            }
             Msg::UpdateTime => {
                 self.time = Body::get_current_time();
                 true
@@ -104,15 +60,12 @@ impl Component for Body {
     fn view(&self) -> Html {
         html! {
             <>
-                <div class="row justify-content-end">
-                    <div class="col-sm-8"></div>
-                    <div class="col-sm-4">
-                        <button style="display:none" onclick=self.link.callback(|_| Msg::ButtonStart)>
-                            { "Setting" }
-                        </button>
-                    </div>
-                     <div class="col-sm-2">
-                         <p> <font color="black"> { &self.time } </font></p>
+                <div class="container">
+                    <div class="row justify-content-end">
+                        <div class="col-md-10"></div>
+                        <div class="col-md-2">
+                            <p> <font color="black"> { &self.time } </font></p>
+                        </div>
                     </div>
                 </div>
             </>
