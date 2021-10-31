4ce565a0932ae4e18c7a04c771c6e4cdef4779dc
Jovi Hsu <jv.hsu@outlook.com>
2021-10-31 18:48:11 +0800

    fix sometime no local media played

--- src/components/keyboard.rs
+++ src/components/keyboard.rs
@@ -1,58 +1,12 @@
-use std::{
-    collections::HashMap,
-    fmt::{Display, Formatter, Result},
-    slice::Iter,
-};
+use std::collections::HashMap;
 
-use wasm_bindgen::{prelude::Closure, JsCast};
-use web_sys::{AudioBuffer, AudioContext, AudioDestinationNode, HtmlAudioElement};
 use yew::agent::Bridged;
 use yew::services::ConsoleService;
 use yew::{html, Bridge, Component, ComponentLink, Html, ShouldRender};
 
 use crate::common::event_bus::EventBus;
 use crate::common::msg::Key;
-
-macro_rules! audio_url{
-    ($($arg:tt)*) => {
-        format!("http://dict.youdao.com/dictvoice?type={prounc}&audio={word}", $($arg)*)
-    };
-}
-
-#[derive(Clone, Copy)]
-enum Pronunc {
-    AmE = 0,
-    BrE = 1,
-}
-
-impl Pronunc {
-    pub fn iterator() -> Iter<'static, Pronunc> {
-        static PRONUNC_S: [Pronunc; 2] = [Pronunc::AmE, Pronunc::BrE];
-        PRONUNC_S.iter()
-    }
-}
-
-impl Display for Pronunc {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        match self {
-            Pronunc::AmE => write!(f, "Amercan pronunciations"),
-            Pronunc::BrE => write!(f, "British pronunciations"),
-        }
-    }
-}
-
-impl From<u8> for Pronunc {
-    fn from(v: u8) -> Pronunc {
-        match v {
-            0 => Pronunc::AmE,
-            _ => Pronunc::BrE,
-        }
-    }
-}
-
-const SOUND_CLICK: &[u8] = include_bytes!("../content/sound/click.wav");
-const SOUND_CORRECT: &[u8] = include_bytes!("../content/sound/correct.mp3");
-const SOUND_WRONG: &[u8] = include_bytes!("../content/sound/wrong.mp3");
+use crate::components::audioplayer::{AudioPlayer, Pronunc};
 
 const DICT_PROGRAMMER: &str = include_str!("../content/dicts/it-words.json");
 const DICT_CET4: &str = include_str!("../content/dicts/CET4_T.json");
@@ -89,63 +43,10 @@ pub struct Keyboard {
     inputs: String,
     _producer: Box<dyn Bridge<EventBus>>,
     link: ComponentLink<Self>,
-    prounc: Pronunc, // Switch Amercan English and British English
+    audio: AudioPlayer,
 }
 
 impl Keyboard {
-    fn play_audio_from_url(&self, url: &str) {
-        // https://docs.rs/web-sys/0.3.19/web_sys/struct.HtmlAudioElement.html
-        let audio = HtmlAudioElement::new_with_src(url).unwrap();
-        let _ = audio.play().unwrap();
-    }
-
-    fn play_audio_from_array(&self, array: &'static [u8]) {
-        let array_u8: js_sys::Uint8Array = js_sys::Uint8Array::from(array);
-        let array_buf: js_sys::ArrayBuffer = array_u8.buffer();
-
-        // https://docs.rs/web-sys/0.3.19/web_sys/struct.AudioContext.html
-        let audio_ctx = AudioContext::new().unwrap();
-        let song = audio_ctx.create_buffer_source().unwrap();
-        let destination: AudioDestinationNode = audio_ctx.destination();
-
-        let handler = move |buf: AudioBuffer| {
-            let buffer: Option<&AudioBuffer> = Some(&buf);
-
-            song.set_buffer(buffer);
-            song.connect_with_audio_node(destination.as_ref()).unwrap();
-            song.start().unwrap();
-        };
-
-        // shoud be 'static
-        let handle: Box<dyn FnMut(_) + 'static> = Box::new(handler) as Box<dyn FnMut(_)>;
-
-        let cb = Closure::wrap(handle);
-
-        let _ = audio_ctx
-            .decode_audio_data_with_success_callback(&array_buf, cb.as_ref().unchecked_ref())
-            .unwrap();
-
-        // don't forget
-        cb.forget();
-    }
-
-    fn play_correct(&self) {
-        self.play_audio_from_array(SOUND_CORRECT);
-    }
-
-    fn play_wrong(&self) {
-        self.play_audio_from_array(SOUND_WRONG);
-    }
-
-    fn play_word(&self, word: &str) {
-        let word_url = audio_url!(prounc = self.prounc as u8, word = word);
-        self.play_audio_from_url(&word_url);
-    }
-
-    fn play_click(&self) {
-        self.play_audio_from_array(SOUND_CLICK);
-    }
-
     fn view_select_button(&self) -> Html {
         let chapers: Vec<usize> = (1..(self.nr_word / 20 + 1)).collect();
 
@@ -270,7 +171,6 @@ impl Component for Keyboard {
         let nr_word: usize = dict.as_array().unwrap().len();
         let cur_index = 0;
         let cur_chaper = cur_index / 20 + 1;
-        let prounc = Pronunc::AmE; // Amercan English as default
 
         Self {
             dict: dict,
@@ -283,7 +183,7 @@ impl Component for Keyboard {
             start_class: String::from("btn btn-primary btn-sm"),
             _producer: EventBus::bridge(link.callback(Key::SetText)),
             link,
-            prounc,
+            audio: AudioPlayer::new(Pronunc::AmE),
         }
     }
 
@@ -294,7 +194,7 @@ impl Component for Keyboard {
                     return true;
                 }
 
-                self.play_click();
+                self.audio.play_click();
 
                 let chr: char = text.as_bytes()[0] as char;
                 self.inputs.push(chr);
@@ -309,19 +209,21 @@ impl Component for Keyboard {
                             self.cur_index = 0;
                         }
                         self.cur_chaper = self.cur_index / 20 + 1;
-                        self.play_correct();
-                        self.play_word(self.dict[self.cur_index]["name"].as_str().unwrap());
+                        self.audio.play_correct();
+                        self.audio
+                            .play_word(self.dict[self.cur_index]["name"].as_str().unwrap());
                     }
                 } else {
-                    self.play_wrong();
+                    self.audio.play_wrong();
                     self.inputs.clear();
                 }
             }
             Key::SelectProunc(prounc) => {
-                self.prounc = prounc.into();
-                let msg = format!("> select audio type: {}.", self.prounc);
+                self.audio.set_prounc(prounc.into());
+                let msg = format!("> select audio type: {}.", self.audio.prounc());
                 ConsoleService::debug(&msg);
-                self.play_word(self.dict[self.cur_index]["name"].as_str().unwrap());
+                self.audio
+                    .play_word(self.dict[self.cur_index]["name"].as_str().unwrap());
             }
             Key::SelectLevel(level) => {
                 self.cur_level = level;
@@ -358,14 +260,16 @@ impl Component for Keyboard {
                 }
                 self.cur_chaper = self.cur_index / 20 + 1;
 
-                self.play_word(self.dict[self.cur_index]["name"].as_str().unwrap());
+                self.audio
+                    .play_word(self.dict[self.cur_index]["name"].as_str().unwrap());
             }
             Key::Submit => {
                 if self.start_status == String::from("Start") {
                     self.start_status = String::from("Pause");
                     self.start_class = String::from("btn btn-secondary btn-sm");
 
-                    self.play_word(self.dict[self.cur_index]["name"].as_str().unwrap());
+                    self.audio
+                        .play_word(self.dict[self.cur_index]["name"].as_str().unwrap());
                 } else {
                     self.start_status = String::from("Start");
                     self.start_class = String::from("btn btn-primary btn-sm");
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,3 +1,4 @@
+pub mod audioplayer;
 pub mod body;
 pub mod footer;
 pub mod header;
